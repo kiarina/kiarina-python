f4a7a706a2b5ed8fe89acc3a2ac55a24234af9af
kiarina <kiarinadawa@gmail.com>
2025-10-10 10:20:22 +0900

    test: add environment-based config and xfail for D1 client test

--- packages/kiarina-lib-cloudflare-d1/tests/test_sync.py
+++ packages/kiarina-lib-cloudflare-d1/tests/test_sync.py
@@ -1,5 +1,28 @@
-from kiarina.lib.cloudflare.d1 import D1Client, create_d1_client
+import os
+
+import pytest
+
+from kiarina.lib.cloudflare.auth import CloudflareAuthSettings
+from kiarina.lib.cloudflare.d1 import D1Client, D1Settings, create_d1_client
 
 
+@pytest.mark.xfail(
+    "KIARINA_LIB_CLOUDFLARE_AUTH_TEST_ACCOUNT_ID" not in os.environ or
+    "KIARINA_LIB_CLOUDFLARE_AUTH_TEST_API_TOKEN" not in os.environ or
+    "KIARINA_LIB_CLOUDFLARE_D1_TEST_DATABASE_ID" not in os.environ,
+    reason="Cloudflare D1 test settings not set",
+)
 def test_create_d1_client() -> None:
-    ...
+    auth_settings = CloudflareAuthSettings.model_validate(
+        {
+            "account_id": os.environ["KIARINA_LIB_CLOUDFLARE_AUTH_TEST_ACCOUNT_ID"],
+            "api_token": os.environ["KIARINA_LIB_CLOUDFLARE_AUTH_TEST_API_TOKEN"],
+        }
+    )
+
+    settings = D1Settings(
+        database_id=os.environ["KIARINA_LIB_CLOUDFLARE_D1_TEST_DATABASE_ID"],
+    )
+
+    # TODO: Mock
+    # client = create_d1_client()
