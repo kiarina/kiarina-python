b51c59043be978d3415f95f1e2262479224fbe8e
kiarina <kiarinadawa@gmail.com>
2026-06-30 17:15:57 +0900

    test(kiarina-utils-app): simplify reset_state fixture and add to workspace

--- packages/kiarina-utils-app/tests/conftest.py
+++ packages/kiarina-utils-app/tests/conftest.py
@@ -8,10 +8,6 @@ from kiarina.utils.app._services import single_instance
 
 @pytest.fixture(autouse=True)
 def reset_state() -> Iterator[None]:
-    """Reset module-level state before and after each test."""
-    app.reset()
-    single_instance._lock = None
     yield
     single_instance.release()
     app.reset()
-    single_instance._lock = None
